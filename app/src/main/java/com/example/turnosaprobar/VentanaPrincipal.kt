023c67dbe78987f308e8e7a8862d83e4b47844a5
Sergio Lucas <155183770+SergioLucas099@users.noreply.github.com>
2026-03-03 17:05:28 -0500

    first commit

---
+++ app/src/main/java/com/example/turnosaprobar/VentanaPrincipal.kt
@@ -0,0 +1,11 @@
+package com.example.turnosaprobar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VentanaPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana_principal)
+    }
+}
