8dd268ca98075b4a24b85a27898dfc92ebdffbea
Sergio Lucas <155183770+SergioLucas099@users.noreply.github.com>
2026-03-13 11:38:03 -0500

    Nuevo Logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
     }
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
